2d9c7ad167348a115e95535c232fbfcfbb8eba76
PreflightAPI <sales@preflightapi.com>
2020-02-19 22:28:10 +0100

    poprawka

--- demo_php/docs/4.php
+++
@@ -1,9 +0,0 @@
-<div class="card">
-                  <div class="card-body">
-                    <div class="text-wrap p-lg-6">
-                      <h2 class="mt-0 mb-4">How it works</h2>
-                      <p>Below You can find system description</p>
-                      
-                    </div>
-                  </div>
-                </div>
--- demo_php/preflightapi2/data.php
+++
@@ -1,5 +0,0 @@
-<?php
-$klucz2=base64_encode('APIKEY:APIPASS');
-$klient2='preflightapi2';
-$katalog2='tmp';
-?>
