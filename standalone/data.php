ddf89f8dae419a3d7714d051618057190135c8d9
PreflightAPI <sales@preflightapi.com>
2020-12-13 17:10:48 +0100

    Update-2

--- standalone/data.php
+++
@@ -1,5 +0,0 @@
-<?php
-$key=base64_encode('49cf8d7041ffdadca1b80ba1a2957c93:730278225fa1a93e4156012c5c73a56c'); //apipass:apikey
-$folder='standalone'; //a root folder
-$temp='tmp';  //temp upload folder, mostly servers "tmp"
-?>
